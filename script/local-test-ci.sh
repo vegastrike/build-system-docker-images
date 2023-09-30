ea649101682b4550d2adbd318976c4d13fe1fbbc
Benjamen Meyer <bm_witness@yahoo.com>
2023-09-30 00:27:45 -0400

    Enhancement: SDL2 support
    Vega Strike is moving to use SDL2. Add support to the docker images
    for SDL2 in addition to SDL1.
    
    - add a tool to easily check that the script works locally
    - ignore Vi Swap files

---
+++ script/local-test-ci.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export FROM="debian:bullseye"
+export MY_OS_NAME="linux"
+export IS_RELEASE=0
+
+#./script/cibuild
+export DOCKER_IMG_NAME="vegastrike/vega-strike-build-env:$(echo "$FROM" | sed 's/:/_/' | sed 's/\//_/')"
+docker build --build-arg from="$FROM" -t "$DOCKER_IMG_NAME" .
