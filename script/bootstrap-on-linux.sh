d140f28c8a7f0dcba8292d1bd0ac2a59c0cdc7bd
Stephen G. Tuggy <sgt@stephengtuggy.com>
2025-12-11 12:47:29 -0800

    Add Rocky Linux 9.7

--- script/bootstrap-on-linux.sh
+++ script/bootstrap-on-linux.sh
@@ -627,7 +627,7 @@ function bootstrapOnFedora ()
 function bootstrapOnRedHat ()
 {
     case "${LINUX_VERSION_ID}" in
-        "9.6")
+        "9.6"|"9.7")
             if [ "${UPDATE_ALL_SYSTEM_PACKAGES}" -eq 1 ]
             then
                 dnf -y upgrade --refresh
@@ -750,7 +750,7 @@ function bootstrapOnRedHat ()
 function bootstrapOnRockyLinux ()
 {
     case "${LINUX_VERSION_ID}" in
-        "9.6")
+        "9.6"|"9.7")
             if [ "${UPDATE_ALL_SYSTEM_PACKAGES}" -eq 1 ]
             then
                 dnf -y upgrade --refresh
--- script/local-test-ci.sh
+++ script/local-test-ci.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export FROM="rockylinux/rockylinux:10.1"
+export FROM="rockylinux/rockylinux:9.7"
 export IS_RELEASE=0
 
 #./script/cibuild
